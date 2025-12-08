cd5888776894b420ecb350d09e9ae599747c0f49
Tymek <tymek_uramek@wp.pl>
2025-12-08 21:49:14 +0100

    script for deploy CrowdFunding contract to anvil

---
+++ foundry/script/DeployCrowdFunding.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Script.sol";
+import "../src/CrowdFunding.sol";
+
+contract DeployCrowdFunding is Script {
+    function run() external {
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+        
+        //ERC20Mock token contract address
+        CrowdFunding crowdFunding = new CrowdFunding(0x5FbDB2315678afecb367f032d93F642f64180aa3);
+        
+        vm.stopBroadcast();
+        
+        console.log("===========================================");
+        console.log("CrowdFunding Contract Deployed!");
+        console.log("===========================================");
+        console.log("Contract Address:", address(crowdFunding));
+        console.log("Deployer Address:", vm.addr(deployerPrivateKey));
+        console.log("===========================================");
+        
+    }
+}
+
+//contract address 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
