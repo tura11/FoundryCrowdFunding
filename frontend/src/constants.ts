efbb16bd57271d6ddba28a5f0a7676ac8215c038
Tymek <tymek_uramek@wp.pl>
2025-12-09 13:21:41 +0100

    changed strings for correct

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,10 +3,10 @@
 // -------------------------------
 // Contract addresses (current for local Anvil)
 // -------------------------------
-export const chainsToCrowdFunding: Record<number, { tsender: `0x${string}` }> = {
+export const chainsToCrowdFunding: Record<number, { CrowdFunding: `0x${string}` }> = {
   // Local Anvil (chainId: 31337)
   31337: {
-    tsender: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", 
+    CrowdFunding: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", 
   },
 }
 
