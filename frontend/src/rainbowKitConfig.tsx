43467fe0fbf4a84c277d08b460e495020f0d7983
Tymek <tymek_uramek@wp.pl>
2025-12-09 12:36:05 +0100

    formated

--- frontend/src/rainbowKitConfig.tsx
+++ frontend/src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { anvil, zksync } from 'wagmi/chains'
+import { anvil, zksync, base } from 'wagmi/chains'
 
 export default getDefaultConfig({
     appName: "CrowdFunding",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [anvil, zksync],
+    chains: [anvil, zksync, base],
     ssr: false
 })
