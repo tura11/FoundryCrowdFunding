b629b32b9624509673ee85dd03fc54359c5aa8ab
Tymek <tymek_uramek@wp.pl>
2025-12-10 15:44:05 +0100

    creating basic UI

--- foundry/script/Deploy.s.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
-
-import {Script, console} from "forge-std/Script.sol";
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,76 @@
-import Image from "next/image";
+'use client';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useCrowdFunding } from '@/hooks/useCrowdFunding';
+import { useState, useEffect } from 'react';
+import { useAccount } from 'wagmi';
+import { create } from 'domain';
+
+
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+  const { isConnected, address} = useAccount();
+  const {
+    campaignCount,
+    createCampaign,
+    isPending,
+    isConfirming,
+    isConfirmed,
+    error
+  } = useCrowdFunding();
+
+  const [title, setTitle] = useState('');
+  const [goal, setGoal] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState('30');
+
+  useEffect(() => {
+    if (isConfirmed) {
+      setTitle('');
+      setGoal('');
+      setDescription('');
+      setDuration('30');
+      alert('✅ Campaign created successfully!')
+    }
+  }, [isConfirmed]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(!isConnected) {
+      alert('Please connect your wallet');
+      return;
+    }
+    try{
+       const defaultTiers = [
+        {
+          name: 'Bronze',
+          description: 'Basic supporter',
+          minContribution: '10', // 10 USDC
+          maxBackers: 100
+        }
+          
+       ];
+
+       const now = Math.floor(Date.now() / 1000);
+       const campaignEndTime  = now + (parseInt(duration) * 24 * 60 * 60);
+
+
+       const defaultMilestones = [
+        {
+          description: 'First milestone',
+          percentage: 50,
+          deadline: campaignEndTime + (30 * 24 * 60 * 60) // 30 days after end of campaign
+       },
+       {
+          description: 'Final milestone',
+          percentage: 50,
+          deadline: campaignEndTime + (60 * 24 * 60 * 60) // 60 days after end of campaign
+       }
+      ];
+
+      await createCampaign(title, goal,description, parseInt(duration), defaultTiers, defaultMilestones);
+    }
+
+
 }
