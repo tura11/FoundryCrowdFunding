3682f86ccc309a62f6eafeb1c46ad7468cb60f0b
Tymek <tymek_uramek@wp.pl>
2025-12-10 19:20:01 +0100

    Changed decimals for USDC standard

--- foundry/Mock/ERC20Mock.sol
+++ foundry/Mock/ERC20Mock.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract MockUSDC is ERC20 {
     constructor() ERC20("Mock USDC", "USDC") {
+        // Mint 1 million USDC (6 decimals)
         _mint(msg.sender, 1_000_000 * 10**6);
     }
 
+    // Override decimals function
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    // Mint function for testing
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
-
 }
-
-//contract address 0x5FbDB2315678afecb367f032d93F642f64180aa3
-
