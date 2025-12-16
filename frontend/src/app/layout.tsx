4d00946f8d273cf6b7e3942ce51a16cc0f87a89e
Tymek <tymek_uramek@wp.pl>
2025-12-17 00:29:16 +0100

    feat: toast alerts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Web3Provider } from '@/app/Web3Provider';
 
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
